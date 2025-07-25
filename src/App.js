6bb27633defe29c64a2a123b65b6a1359bdb1814
YADIRA JAZMIN PEREZ CASTILLO <yadira.perez@rodac.com.mx>
2025-07-25 09:29:08 -0600

    OKKK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Card from './components/Card';
 
-const data = [
+const data1 = [
   { titulo: "React", descripcion: "Una librería para construir interfaces de usuario." },
   { titulo: "JSX", descripcion: "Sintaxis que combina HTML con JavaScript." },
   { titulo: "Hooks", descripcion: "Funciones para manejar estado y ciclo de vida en componentes funcionales." }
 ];
+
+
+const dataz = [
+  { titulo: "Tarjeta Base", descripcion: "Esta es la tarjeta de ejemplo." }
+];
+
+const data = [
+  { titulo: "Tarjeta Base", descripcion: "Esta es la tarjeta de ejemplo." },
+  { titulo: "Juan", descripcion: "Soy parte del equipo 1." },
+  { titulo: "María", descripcion: "Mi tarjeta personalizada." }
+];
+
 
 function App() {
   return (
